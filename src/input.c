38c1b7f2987152e75682b51ca93cb4874462c1d6
fuguenot <fuguenot@gmail.com>
2025-05-01 18:31:15 -0700

    Limit movement

--- src/input.c
+++ src/input.c
@@ -1,27 +1,36 @@
 #include "inspect.h"
 
+#include <math.h>
+#include <string.h>
+
 #include <ncurses.h>
 
 void handle_events() {
     int c = getch();
-        switch (c) {
-        case 'q': running = FALSE; break;
-        case 'h':
-            if (col > 0) col--;
-            redraw_bar_needed = TRUE;
-            break;
-        case 'j':
+    switch (c) {
+    case 'q': running = FALSE; break;
+    case 'h':
+        if (col > 0) col--;
+        redraw_bar_needed = TRUE;
+        break;
+    case 'j':
+        if (row + drow < clines - 1) {
             row++;
-            redraw_bar_needed = TRUE;
-            break;
-        case 'k':
-            if (row > 0) row--;
-            redraw_bar_needed = TRUE;
-            break;
-        case 'l':
-            col++;
-            redraw_bar_needed = TRUE;
-            break;
-        default: break;
+            col = fminf(col, strlen(lines[row + drow]));
         }
+        redraw_bar_needed = TRUE;
+        break;
+    case 'k':
+        if (row > 0) {
+            row--;
+            col = fminf(col, strlen(lines[row + drow]));
+        }
+        redraw_bar_needed = TRUE;
+        break;
+    case 'l':
+        if (col < strlen(lines[row + drow])) col++;
+        redraw_bar_needed = TRUE;
+        break;
+    default: break;
+    }
 }
